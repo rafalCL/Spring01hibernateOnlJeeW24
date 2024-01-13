82ad0eb7ed6b2ffbfc7f360dd6fda798e569fa65
edu <edu@mail.pl>
2024-01-13 14:27:51 +0100

    encje z1 Book encja

---
+++ src/main/java/pl/coderslab/spring01hibernateonljeew24/entity/Book.java
@@ -0,0 +1,42 @@
+package pl.coderslab.spring01hibernateonljeew24.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private int rating;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
