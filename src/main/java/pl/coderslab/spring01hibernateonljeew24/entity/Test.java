84c73318a468327ef379a01839584bf54ae2f55d
edu <edu@mail.pl>
2024-01-13 11:33:19 +0100

    initialize hibernate in project

---
+++ src/main/java/pl/coderslab/spring01hibernateonljeew24/entity/Test.java
@@ -0,0 +1,14 @@
+package pl.coderslab.spring01hibernateonljeew24.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Test {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
---
+++ src/main/resources/META-INF/sql/db.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE bookstore_w_24
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_unicode_ci;
