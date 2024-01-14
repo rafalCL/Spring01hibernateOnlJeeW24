86f6d5c120074e88f4d367fa19d40e7c143ce662
edu <edu@mail.pl>
2024-01-14 12:50:33 +0100

    d2 form binding z1 part 2 WITH binding for Person and also edit Person

--- src/main/java/pl/coderslab/spring01hibernateonljeew24/controller/PersonController.java
+++ src/main/java/pl/coderslab/spring01hibernateonljeew24/controller/PersonController.java
@@ -1,6 +1,7 @@
 package pl.coderslab.spring01hibernateonljeew24.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import pl.coderslab.spring01hibernateonljeew24.entity.Person;
 
@@ -21,6 +22,31 @@ public class PersonController {
     ) {
         Person person = new Person(login, password, email);
         // todo save to db
+        return person.toString();
+    }
+
+    @GetMapping("/addFormBind")
+    public String addFormBind(Model m) {
+        m.addAttribute("person", new Person());
+        return "/person/addFormBind";
+    }
+
+    @PostMapping("/addFormBind")
+    @ResponseBody
+    public String postAddFormBind(@ModelAttribute Person person) {
+        return person.toString();
+    }
+
+// below binding with already prefilled object
+    @GetMapping("/edit")
+    public String editPerson(Model m) {
+        m.addAttribute("person", new Person("somelogin", "somepassword", "someemail"));
+        return "/person/addFormBind";
+    }
+
+    @PostMapping("/edit")
+    @ResponseBody
+    public String postEdit(@ModelAttribute Person person) {
         return person.toString();
     }
 }
---
+++ src/main/webapp/WEB-INF/views/person/addFormBind.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<html>
+<head>
+    <title>Add person</title>
+</head>
+<body>
+    <form:form method="post" modelAttribute="person">
+        <div><label for="login">login<form:input id="login" path="login"/></label></div>
+        <div><label for="password">password<form:password id="password" path="password"/></label></div>
+        <div><label for="email">email<form:input id="email" path="email"/></label></div>
+        <div><input type="submit"></div>
+    </form:form>
+</body>
+</html>
